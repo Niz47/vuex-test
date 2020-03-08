0c11e46a702e9afd4a32cf23ab9cbd70e983e8d3
Niz47 <zinmarhtun47@gmail.com>
2020-03-08 01:30:27 -0800

    add two components

---
+++ src/components/product-list-two.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="product-list-two">
+    <h2>Product List Two</h2>
+    <ul>
+      <li v-for="product in products">
+        <span class="name">{{product.name}}</span>
+        <span class="price">${{product.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['products'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+#product-list-two{
+  background: #FFF8B1;
+  box-shadow: 1px 2px 3px rgb(0, 0, 0,0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-two ul{
+  padding: 0;
+  list-style-type: disc;
+}
+#product-list-two li{
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: #E8800C;
+}
+</style>
